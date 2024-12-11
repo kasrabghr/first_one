be8f25f7cb32c44dd86322ac43ca3762ed887077
kasrabghr <kasra.bghr@gmail.com>
2024-12-11 17:56:52 -0500

    Add files via upload

---
+++ Game 3.py
@@ -0,0 +1,20 @@
+import random
+a= -1
+b=random.randint(1,99)
+c=1
+z=99
+while a!=b:
+    print ("is your number ",b,"?")
+    r=input()
+    if r=="b":
+        c=b
+        b=random.randint(c,z)
+    elif r=="k":
+        z=b
+        b=random.randint(c,b)
+    elif r=="d":
+        print("Yeaaaaay!!, your number is: ",b) 
+        break
+   
+  
+
